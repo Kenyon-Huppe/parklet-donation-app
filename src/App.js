7cc1dd891fab8064469a67112b645c2c2d5bfa9b
Kenyon Huppe <kenyonhuppe@gmail.com>
2023-05-01 18:34:13 -0700

    Organized code
    - Deleted Button component (redundant)
    - Added several components that are required for the layout of the landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import Header from './Components/landingPage/Header';
 import ProjectName from './Components/landingPage/ProjectName';
+import SectionTitle from './Components/landingPage/SectionTitle';
+import classes from './CSS/landing.module.css'
 
 function App() {
   return (
     <div className="App">
+      {/* opening section */}
       <Header></Header>
-      <img src="" alt="" className='banner-img' />
+      <img src="" alt="" className="banner-img" />
       <ProjectName></ProjectName>
+
+      <div className="project-section">
+        <SectionTitle></SectionTitle>
+        <p></p>
+        <img src="" alt="" />
+      </div>
+      <div className="history-section">
+
+      </div>
+
+      <div className="footer-section"></div>
+
     </div>
   );
 }
---
+++ src/Components/landingPage/SectionTitle.js
@@ -0,0 +1,14 @@
+// the title for each section before the content
+import React from 'react'
+import classes from '../../CSS/landing.module.css'
+
+
+const SectionTitle = () => {
+    return (
+        <div>SectionTitle</div>
+    )
+}
+
+export default SectionTitle
+
+// todo include divider under title
