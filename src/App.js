c04f6c990b287617eee9e3773bf6c5613a549e53
Kenyon Huppe <kenyonhuppe@gmail.com>
2023-05-04 13:32:31 -0700

    Updated images & favicons
    Replaced the placeholder content with the actual content for the site.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
 import './App.css';
 import Header from './Components/landingPage/Header';
-import ProjectName from './Components/landingPage/ProjectName';
 import SectionTitle from './Components/landingPage/SectionTitle';
 import Card from './Components/Card';
 import classes from './CSS/landing.module.css'
+import facebookIcon from './images/facebook.svg'
+import twitterIcon from './images/twitter.svg'
+import instagramIcon from './images/instagram.svg'
 
 function App() {
   return (
     <div className="App">
-      <div className={classes.bannerSection}>
+      <section className={classes.bannerSection}>
         <Header></Header>
         <img src={require('./images/UDK-banner.jpg')} alt='Banner-Image' className={classes.bannerImg} />
-        <ProjectName></ProjectName>
-      </div>
-      <div className={classes.projectSection}>
+        <img src={require('./images/UDK-parklet-name.png')} alt="Partlet-on-the-Ave" className={classes.title} />
+      </section>
+      <section className={classes.projectSection}>
         <SectionTitle titleName={'About the Parklet'}></SectionTitle>
-        <p>Text about project HERE</p>
+        <p>
+          To help fund the renovation of The Parklet on the Ave., we invite you to become a part of this exciting project and join us in making a difference in our community. Your support will be acknowledged with a wooden floor-board personalized with a custom quote by you (of up to 80 words), showcasing your commitment to the parklet's continued success and making your mark on the U-District.
+        </p>
+        <p>
+          So, what are you waiting for? Donate today and be a part of something truly special. Your name will forever be associated with the parklet on the Ave, a testament to your dedication to the community and its public spaces. Thank you for your support!
+        </p>
+
         <img src={require('./images/UDK-project.jpg')} alt='Parklet-Project-Img' />
-      </div>
-      <div className={classes.historySection}>
+      </section>
+      <section className={classes.historySection}>
         <SectionTitle titleName={'Parklet History'}></SectionTitle>
         <div className={classes.cardHolder}>
           {/* !!!!!!! TODO PUT IN CORRECT IMAGES!!!!!!! */}
-          <Card imageLink={require('./images/UDK-Parklet-01.jpg')} cardTitle={'CARD1 TITLE HERE'} cardText={'CARD1 TEXT HERE'}></Card>
-          <Card imageLink={require('./images/UDK-Parklet-01.jpg')} cardTitle={'CARD2 TITLE HERE'} cardText={'CARD2 TEXT HERE'}></Card>
-          <Card imageLink={require('./images/UDK-Parklet-01.jpg')} cardTitle={'CARD3 TITLE HERE'} cardText={'CARD3 TEXT HERE'}></Card>
+          <Card imageLink={require('./images/kickstarter-2-logo-png-transparent.png')} cardTitle={'Original Kickstarter'} cardText={'The kickstarter campaign that started it all!'}></Card>
+          <Card imageLink={require('./images/UDK-parklet-dedication.jpg')} cardTitle={'Parklet Dedication'} cardText={'Original dedication of parklet (May 2015)'}></Card>
+          <Card imageLink={require('./images/UDK-parklet-contstruction.jpg')} cardTitle={'Construction Timelapse'} cardText={'Short timelapse video of the parklet construction'}></Card>
         </div>
-      </div>
-      <div className={classes.footerSection}>
+      </section>
+      <section className={classes.footerSection}>
         <div className={classes.leftFooter}>
           {/* !!! TODO ADD LINKS BELOW !!! */}
           <ul className={classes.links}>
-            <li><a href="">Facebook</a></li>
-            <li><a href="">Twitter</a></li>
-            <li><a href="">Instagram</a></li>
+            <li>
+              <a href=''><img src={facebookIcon} alt="Facebook" /></a>
+            </li>
+            <li>
+              <a href=""><img src={twitterIcon} alt="Twitter" /></a>
+            </li>
+            <li>
+              <a href=""><img src={instagramIcon} alt="Instagram" /></a>
+            </li>
           </ul>
           <h3 className={classes.footerTitle}>Make your mark on the park!</h3>
-          <p className={classes.footerText}>Get a custom inscription on a parklet deck board with a donation of $100 or just kick in a little bit to support our non-profit grow!</p>
+          <p className={classes.footerText}>
+            Get a custom inscription on a parklet deck board with a donation of $100 or just kick in a little bit to support our non-profit grow!
+          </p>
           <button className={`${classes.btn} ${classes.footerBtn}`}><a href=''>Donate Now!</a></button>
-          <div className={classes.contactInfo}>
+          <article className={classes.contactInfo}>
             <div className={classes.phoneInfo}>
               <p>Phone</p>
               <p>PHONE NUMBER HERE</p>
@@ -47,14 +63,24 @@ function App() {
               <p>Email</p>
               <p>EMAIL ADDRESS HERE</p>
             </div>
-          </div>
+          </article>
         </div>
         <div className={classes.rightFooter}>
-          {/* !!! TODO REPLACE IMG HERE !!! */}
-          <img src={require('./images/UDK-Parklet-01.jpg')} alt="Footer-Img" />
+          <img src={require('./images/UDK-board.jpg')} alt="Footer-Img" />
         </div>
-      </div>
-
+        {/* below is included as it is necessary to credit for use of given favicons */}
+        <article className={classes.faviconLink}>
+          <div className={classes.faviconLinkFacebook}>
+            <a target="_blank" href="https://icons8.com/icon/118467/facebook">Facebook</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+          </div>
+          <div className={classes.faviconLinkTwitter}>
+            <a target="_blank" href="https://icons8.com/icon/60014/twitter">Twitter</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+          </div>
+          <div className={classes.faviconLinkInstagram}>
+            <a target="_blank" href="https://icons8.com/icon/32309/instagram">Instagram</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+          </div>
+        </article>
+      </section>
     </div >
   );
 }
