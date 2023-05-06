86c70a01bfde65458eb7f2bf6334fe8cc2719d77
Brynn Zehnder <70238835+Brynn-z@users.noreply.github.com>
2023-05-06 16:20:17 -0700

    Added CSS to nav bar

--- src/Components/landingPage/Header.js
+++ src/Components/landingPage/Header.js
@@ -3,13 +3,16 @@ import React from 'react'
 // import { useNavigate } from 'react-router-dom'
 import classes from '../../CSS/landing.module.css'
 
+
 const Header = ({ homePageLink, donationPageLink }) => {
     // const navigate = useNavigate();
     return (
         <div className={classes.header}>
             <img src={require('../../images/UDK-logo.png')} alt="Logo" className={classes.logo} onClick={homePageLink} />
-
-            <button className={`${classes.btn} ${classes.donateBtn}`} onClick={donationPageLink}>Donate</button>
+           <div className={classes.buttonNav}>
+                <button className={`${classes.btn} ${classes.donateBtn}`} onClick={donationPageLink}>Donate</button>
+            </div>
+            {/* <hr></hr> */}
         </div>
     )
 }
