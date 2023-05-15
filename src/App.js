0d79fbaba7a4a856efc33d29be4685d7408fb94d
Brynn Zehnder <70238835+Brynn-z@users.noreply.github.com>
2023-05-14 19:01:38 -0700

    Added links to the landing page card components and touched-up the CSS

--- src/App.js
+++ src/App.js
@@ -62,9 +62,9 @@ const customDonationLink = () => {
       <h1 className={classes.titleNameHis}>Parklet History</h1>
       <div className={classes.cardHolder}>
         {/* !!!!!!! TODO PUT IN CORRECT IMAGES!!!!!!! */}
-        <Card cardDescription={'Original Kickstarter'} imageLink={require('./images/kickstarter-2-logo-png-transparent.png')} cardText={'The kickstarter campaign that started it all!'}></Card>
-        <Card cardDescription={'Parklet Dedication'} imageLink={require('./images/UDK-parklet-dedication.jpg')} cardText={'Original dedication of parklet (May 2015)'}></Card>
-        <Card cardDescription={'Construction Timelapse'} imageLink={require('./images/UDK-parklet-contstruction.jpg')} cardText={'Short timelapse video of the parklet construction'}></Card>
+        <Card pageLink={"https://www.kickstarter.com/projects/udistrictsquare/u-district-parklet"} cardDescription={'Original Kickstarter'} imageLink={require('./images/kickstarter-2-logo-png-transparent.png')} cardText={'The kickstarter campaign that started it all!'}></Card>
+        <Card pageLink={"https://www.theurbanist.org/2015/06/26/u-district-parklet-launch-celebration/"} cardDescription={'Parklet Dedication'} imageLink={require('./images/UDK-parklet-dedication.jpg')} cardText={'Original dedication of parklet (May 2015)'}></Card>
+        <Card pageLink={"https://www.youtube.com/watch?v=0TpebWOzGE4"} cardDescription={'Construction Timelapse'} imageLink={require('./images/UDK-parklet-contstruction.jpg')} cardText={'Short timelapse video of the parklet construction'}></Card>
       </div>
     </div>
     <div className={classes.footerSection}>
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,14 +3,13 @@ import React from 'react'
 import SectionTitle from './landingPage/SectionTitle'
 import classes from '../CSS/landing.module.css'
 
-const Card = ({ imageLink, cardTitle, cardDescription, cardText, pageLink, directoryChange }) => {
+const Card = ({ imageLink, cardDescription, cardText, pageLink, directoryChange }) => {
     return (
         <div className={classes.card}>
             <a href={pageLink}>
                 <img className={classes.cardImg} src={imageLink} alt="Image-Link" onClick={directoryChange} width={300} />
                 <p className={classes.cardDescription}>{cardDescription}</p>
                 <hr></hr>
-                {/* <SectionTitle className={classes.cardSectionTitle}>{cardTitle}</SectionTitle> */}
                 <p className={classes.cardText}>{cardText}</p>
             </a>
         </div>
